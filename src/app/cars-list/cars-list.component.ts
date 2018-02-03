da2682e85b6f305d2841adba6980818e79b690db
Justyna Korczynska Kadziel <s00132250@mail.itsligo.ie>
2018-02-03 15:09:15 +0000

    Display a list of cars

--- src/app/cars-list/cars-list.component.ts
+++ src/app/cars-list/cars-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Car } from '../models/car';
 
 @Component({
   selector: 'app-cars-list',
@@ -6,7 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cars-list.component.css']
 })
 export class CarsListComponent implements OnInit {
-
+  cars : Car[] = [
+  {
+    id: 1,
+    model: 'Mazda',
+    //type: '',
+    plate: 'AA',
+    deliveryDate: '02-02-2018',
+    deadline: '03-02-2018',
+    //color: '',
+    //power: '',
+    clientFirstName: 'John',
+    clientSurname: 'Doe',
+    cost: 300,
+    isFullyDamaged: true,
+    //year: ''
+  },
+  {
+    id: 2,
+    model: 'Mazda',
+    //type: '',
+    plate: 'AA',
+    deliveryDate: '02-02-2018',
+    deadline: '03-02-2018',
+    //color: '',
+    //power: '',
+    clientFirstName: 'Mike',
+    clientSurname: 'Doe',
+    cost: 300,
+    isFullyDamaged: true,
+    //year: ''
+  }
+  ]
   constructor() { }
 
   ngOnInit() {
