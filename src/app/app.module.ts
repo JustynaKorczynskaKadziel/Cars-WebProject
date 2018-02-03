63194af71aac2dd03baf849cd4aa28a407367449
Justyna Korczynska Kadziel <s00132250@mail.itsligo.ie>
2018-02-03 14:15:56 +0000

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CarsListComponent } from './cars-list/cars-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarsListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
