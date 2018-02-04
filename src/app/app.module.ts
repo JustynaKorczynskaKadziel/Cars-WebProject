d78114aa8ac53f49a5ad0ab9d8f0165f40bcb718
Justyna Korczynska Kadziel <s00132250@mail.itsligo.ie>
2018-02-04 00:29:21 +0000

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
 import { CarsComponent } from './cars/cars.component';
 import { CarsListComponent } from './cars/cars-list/cars-list.component';
 import { TotalCostComponent } from './cars/cars-list/total-cost/total-cost.component';
 import { AddCarComponent } from './cars/add-car/add-car.component';
 import { SearchComponent } from './cars/search/search.component';
 import { SharedModule } from './shared-module/shared-module.module';
+
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
@@ -23,7 +26,12 @@ import { SharedModule } from './shared-module/shared-module.module';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    SharedModule
+    SharedModule,
+    RouterModule.forRoot(
+      [
+        {path: '', redirectTo: 'cars', pathMatch: 'full' },
+        {path: 'cars', component: <any>CarsListComponent}]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
