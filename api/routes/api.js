abaeae4874dbfc5321aa2565ae6ea92344ce78f5
Justyna Korczynska Kadziel <s00132250@mail.itsligo.ie>
2018-02-06 10:49:13 +0000

    Create api file

---
+++ api/routes/api.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const mongojs = require("mongojs");
+var db = mongojs('cars', ['cars'])
+
+//Connect
+const connection = (closure) => {
+    return MongoClient.connect('mongodb://localhost:27017/cars', (err, db) => {
+        if (err){
+             return console.log(err);
+        }
+        var db = client.db('cars');
+        closure;
+    })
+};
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+module.exports = router;
